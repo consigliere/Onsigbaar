6746c90d8f5a6f195ccdf88d53189b973cf2e67d
anonymoussc <50c5ac69@opayq.com>
2017-06-05 07:22:17 +0700

    Modified app/Components/Onsigbaar/src/Http/routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,5 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'onsigbaar', 'namespace' => 'App\\Components\Onsigbaar\Http\Controllers'], function()
-{
+Route::group(['middleware' => 'web', 'prefix' => 'onsigbaar', 'namespace' => 'App\Components\Onsigbaar\Http\Controllers'], function () {
     Route::get('/', 'OnsigbaarController@index');
 });
