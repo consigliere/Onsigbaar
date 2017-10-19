69db7bfb76166a9cddd3fdbc408a8ca8579de6a9
anonymoussc <50c5ac69@opayq.com>
2017-10-20 06:52:34 +0700

    Modified OnsigbaarServiceProvider

--- src/Providers/OnsigbaarServiceProvider.php
+++ src/Providers/OnsigbaarServiceProvider.php
@@ -36,6 +36,7 @@ class OnsigbaarServiceProvider extends ServiceProvider
     {
         $this->app->register(\App\Components\Signature\Providers\SignatureServiceProvider::class);
         $this->app->register(\App\Components\Siegnor\Providers\SiegnorServiceProvider::class);
+        $this->app->register(\Optimus\ApiConsumer\Provider\LaravelServiceProvider::class);
     }
 
     /**
