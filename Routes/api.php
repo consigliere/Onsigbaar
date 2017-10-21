da9bac740cb5a5f4b499db0d904900cd6868c2d2
anonymoussc <50c5ac69@opayq.com>
2017-10-22 03:02:18 +0700

    Modified api \Routes

--- Routes/api.php
+++ Routes/api.php
@@ -4,15 +4,6 @@
  * Created by @anonymoussc on 6/27/2017 12:27 PM.
  */
 
-Route::group(['middleware' => 'api', 'prefix' => '', 'namespace' => 'App\Components\Onsigbaar\Http\Controllers\Auth'], function () {
-    Route::post('/login', 'LoginController@login');
-    Route::post('/login/refresh', 'LoginController@refresh');
-});
-
-Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => 'App\Components\Onsigbaar\Http\Controllers\Auth'], function () {
-    Route::post('/logout', 'LoginController@logout');
-});
-
 Route::group(['middleware' => 'api', 'prefix' => 'onsigbaar', 'namespace' => 'App\Components\Onsigbaar\Http\Controllers'], function () {
     //
 });
