2cb345fc299c713be233d22d9aa8b6ea99aaaf80
anonymoussc <50c5ac69@opayq.com>
2017-10-22 07:39:35 +0700

    Modified web Routes

--- Routes/web.php
+++ Routes/web.php
@@ -5,5 +5,5 @@
  */
 
 Route::group(['middleware' => 'web', 'prefix' => 'onsigbaar', 'namespace' => 'App\Components\Onsigbaar\Http\Controllers'], function () {
-    Route::get('/', 'OnsigbaarController@index');
+    // Route::get('/', 'OnsigbaarController@index');
 });
